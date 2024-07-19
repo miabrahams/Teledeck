68a0868355104143968d9005afc1ccd800997b30
Michael Abrahams <miabraha@gmail.com>
2024-07-19 09:33:51 -0400

    Add GORM logging

--- server/internal/store/db/db.go
+++ server/internal/store/db/db.go
@@ -2,10 +2,15 @@ package db
 
 import (
 	// "goth/internal/store"
+	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/sqlite" // Sqlite driver based on CGO
+	"gorm.io/gorm/logger"
+
 	// "github.com/glebarez/sqlite" // Pure go SQLite driver, checkout https://github.com/glebarez/sqlite for details
+
 	"gorm.io/gorm"
 )
 
@@ -17,7 +22,21 @@ func open(dbName string) (*gorm.DB, error) {
 		return nil, err
 	}
 
-	return gorm.Open(sqlite.Open(dbName), &gorm.Config{})
+	/* TODO - logging config */
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,        // Don't include params in the SQL log
+			Colorful:                  false,       // Disable color
+		},
+	)
+
+	return gorm.Open(sqlite.Open(dbName), &gorm.Config{
+		Logger: newLogger,
+	})
 }
 
 func MustOpen(dbName string) *gorm.DB {
