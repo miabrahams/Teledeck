54216575a8c6eac02a375e3f8d7201907077d995
Michael Abrahams <miabraha@gmail.com>
2024-07-21 22:03:28 -0400

    Add right click context menu

--- server/static/script/gallery.js
+++ server/static/script/gallery.js
@@ -1,19 +1,59 @@
 
-window.fullscreenView = null;
 
-document.addEventListener('click', function(e) {
+function clampInsideScope(mouseX, mouseY, scope, innerElement) {
+  const { left: scopeOffsetX, top: scopeOffsetY, width: scopeWidth, height: scopeHeight } = scope.getBoundingClientRect();
+  const innerWidth = innerElement.clientWidth;
+  const innerHeight = innerElement.clientHeight;
+
+  const clampedX = Math.min(Math.max(mouseX, scopeOffsetX), scopeOffsetX + scopeWidth - innerWidth);
+  const clampedY = Math.min(Math.max(mouseY, scopeOffsetY), scopeOffsetY + scopeHeight - innerHeight);
+  return { clampedX, clampedY };
+}
+
+function setupContextMenu() {
+  const scope = document.querySelector('main');
+  const contextMenu = document.getElementById('context-menu');
+
+  scope.addEventListener('contextmenu', (contextEvent) => {
+    contextEvent.preventDefault();
+    const { clientX, clientY } = contextEvent;
+    const { clampedX, clampedY } = clampInsideScope(clientX, clientY, scope, contextMenu);
+
+    contextMenu.style.left = `${clampedX}px`;
+    contextMenu.style.top = `${clampedY}px`;
+
+    contextMenu.classList.remove('visible');
+    setTimeout(() => { contextMenu.classList.add('visible'); });
+
+    // const mediaItem = contextEvent.target.closest('media-item');
+  });
+
+  document.addEventListener('click', (clickEvent) => {
+    if (clickEvent.target.offsetParent != contextMenu) {
+      contextMenu.classList.remove('visible');
+    }
+  });
+}
+
+function setupModalView() {
+  window.fullscreenView = null;
+  document.addEventListener('click', (clickEvent) => {
     if (window.fullscreenView !== null) {
-        console.log("Closing modal");
-        console.log(window.fullscreenView.restoreTarget)
-        window.fullscreenView.restoreTarget.appendChild(window.fullscreenView.mediaItem);
-        window.fullscreenView.remove();
-        window.fullscreenView = null;
-        return e.preventDefault();
+      console.log('Closing modal');
+      console.log(window.fullscreenView.restoreTarget);
+      window.fullscreenView.restoreTarget.appendChild(
+        window.fullscreenView.mediaItem
+      );
+      window.fullscreenView.remove();
+      window.fullscreenView = null;
+      return clickEvent.preventDefault();
     }
 
-    const mediaContainer = e.target.closest('[data-fullscreen="true"]');
-    if (mediaContainer && !e.target.closest('.download-button')) {
-      console.log("Clicked on media content");
+    const mediaContainer = clickEvent.target.closest(
+      '[data-fullscreen="true"]'
+    );
+    if (mediaContainer && !clickEvent.target.closest('.download-button')) {
+      console.log('Clicked on media content');
       const fullscreenView = document.createElement('div');
       fullscreenView.className = 'fullscreen-view';
       fullscreenView.innerHTML = `<div class="fullscreen-content"></div>`;
@@ -23,17 +63,27 @@ document.addEventListener('click', function(e) {
       const mediaItem = mediaContainer.children[0];
       fullscreenView.appendChild(mediaItem);
       fullscreenView.restoreTarget = mediaContainer;
-      fullscreenView.mediaItem = mediaItem
+      fullscreenView.mediaItem = mediaItem;
       window.fullscreenView = fullscreenView;
     }
-});
+  });
+}
 
+function setupAutoplay() {
+  window.addEventListener('load', function () {
+    for (video of document.querySelectorAll('video')) {
+      video.muted = 1;
+      video.addEventListener('mouseover', video.play);
+      video.addEventListener('mouseout', video.pause);
+      video.addEventListener('click', () => {
+        video.muted = 0;
+      });
+    }
+  });
+}
 
-
-// Autoplay on hover
-window.addEventListener("load", function() {
-  for (video of document.querySelectorAll("video")) {
-    video.addEventListener("mouseover", video.play)
-    video.addEventListener("mouseout", video.pause)
-  }
-});
+(function () {
+  setupContextMenu();
+  setupModalView();
+  setupAutoplay();
+})();
