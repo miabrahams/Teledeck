8aa1334c612134c62a281e675193c5521c204fd3
Michael Abrahams <miabraha@gmail.com>
2024-07-26 20:26:15 -0400

    Refactor admin scripts

--- admin/admin.py
+++ admin/admin.py
@@ -1,12 +1,18 @@
 import json
 from sqlmodel import create_engine, Session, select
-from sqlalchemy.engine.base import Engine
 from dotenv import load_dotenv
 import pathlib
 from models.telegram import Tag, MediaItem
 import argparse
 from os import environ
 from tqdm import tqdm
+from lib.tl_client import get_context, get_messages, extract_forward
+from telethon.utils import resolve_id, get_peer_id
+from telethon import functions, types, hints, tl
+from telethon.tl.custom.dialog import Dialog
+import asyncio
+from typing import cast
+
 
 load_dotenv()
 
@@ -45,17 +51,44 @@ def find_orphans(media_path: pathlib.Path, orphan_path: pathlib.Path):
                 item.rename(orphan_path.joinpath(name))
                 print(f"No file found: {name}" )
 
+async def save_forwards(chat_name: str):
+    ctx = await get_context()
+    # resolved = get_peer_id(chat_id)
+    entity: Optional[hints.Entity] = None
+    async for d in ctx.tclient.iter_dialogs():
+        dialog = cast(Dialog, d)
+        if dialog.name == chat_name:
+            entity: hints.Entity = dialog.entity
+            break
+    if not entity:
+        print("Could not find chat.")
+
+    n = 0
+    async for message in ctx.tclient.iter_messages(entity, limit=5000):
+        try:
+            await extract_forward(ctx, message)
+        except Exception as e:
+            print(e)
+        n += 1
+        print(n)
+
+    ctx.save_data()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Custom commands')
     parser.add_argument('--add-tags', action='store_true', help='Add tags to the database')
     parser.add_argument('--find-orphans', action='store_true', help='Find files not associated with an entry.')
+    parser.add_argument('--save-forwards', help='Find files not associated with an entry.')
     args = parser.parse_args()
 
     if args.add_tags:
         add_tags_to_database()
 
-    if args.find_orphans:
+    elif args.find_orphans:
         directory_path = 'static/media'
         orphan_path = 'recyclebin/orphan'
         find_orphans(pathlib.Path(directory_path), pathlib.Path(orphan_path))
+
+    elif args.save_forwards:
+        asyncio.get_event_loop().run_until_complete(save_forwards(args.save_forwards))
