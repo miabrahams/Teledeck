9f4adc9ecdbec8e97a289c0e0b9147dc54db4908
Michael Abrahams <miabraha@gmail.com>
2024-11-02 08:50:47 -0400

    Split MediaCard into sub components

--- web/src/components/MediaCard.tsx
+++ web/src/components/MediaCard.tsx
@@ -18,8 +18,11 @@ const VideoItem: React.FC<{ item: MediaItem }> = ({ item }) => {
 
   const handleVideoToggle = () => {
     if (!vidRef.current) return;
-    if (isPlaying) { vidRef.current.pause(); }
-    else { vidRef.current.play(); }
+    if (isPlaying) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
     setIsPlaying(!isPlaying);
   };
 
@@ -52,51 +55,82 @@ const VideoItem: React.FC<{ item: MediaItem }> = ({ item }) => {
 
 
 const MediaInfo: React.FC<{ item: MediaItem }> = ({ item }) => {
-
-      return (
-      <div className="media-info">
-        <div className="flex justify-between items-start mb-2">
-          <h2 className="media-meta media-filename"> {item.file_name} </h2>
-          {item.favorite && ( <Star className="w-5 h-5 text-yellow-500 fill-current" />)}
-        </div>
-
-        <div className="media-meta media-channel">
-          <p>Channel: {item.channelTitle}</p>
-        </div>
-        <p className="media-meta media-date">Date: {new Date(item.created_at).toLocaleDateString()}</p>
-        {item.TelegramText && <p className="media-meta media-text truncate">{item.TelegramText}</p>}
-        </div>
-    )};
-
-const MediaControls: React.FC<{ item: MediaItem, isHovering: boolean, onFavorite: Function, onDelete: Function }> = ({ item, isHovering, onFavorite, onDelete }) => {
-
-  const controlClass = `controls ${isHovering ? 'opacity-100' : 'opacity-0'} transition-opacity duration-200`
   return (
-      <div className={controlClass} >
-        <button
-          onClick={() => window.open(`/media/${item.file_name}`, '_blank')}
-          className="p-2 bg-gray-900 bg-opacity-75 rounded-full text-white hover:bg-opacity-90"
-        >
-          <Download className="w-4 h-4" />
-        </button>
-        <button
-          onClick={() => onFavorite(item)}
-          className="p-2 bg-gray-900 bg-opacity-75 rounded-full text-white hover:bg-opacity-90"
-        >
-          <Star
-            className={`w-4 h-4 ${item.favorite ? 'fill-yellow-500' : ''}`}
-          />
-        </button>
-        {!item.favorite && (
-          <button
-            onClick={() => onDelete(item)}
-            className="p-2 bg-gray-900 bg-opacity-75 rounded-full text-white hover:bg-opacity-90"
-          >
-            <Trash className="w-4 h-4" />
-          </button>
+    <div className="media-info">
+      <div className="flex justify-between items-start mb-2">
+        <h2 className="media-meta media-filename"> {item.file_name} </h2>
+        {item.favorite && (
+          <Star className="w-5 h-5 text-yellow-500 fill-current" />
         )}
       </div>
-  )};
+
+      <div className="media-meta media-channel">
+        <p>Channel: {item.channelTitle}</p>
+      </div>
+      <p className="media-meta media-date">
+        Date: {new Date(item.created_at).toLocaleDateString()}
+      </p>
+      {item.TelegramText && (
+        <p className="media-meta media-text truncate">{item.TelegramText}</p>
+      )}
+    </div>
+  );
+};
+
+const MediaControls: React.FC<{
+  item: MediaItem;
+  isHovering: boolean;
+  onFavorite: Function;
+  onDelete: Function;
+}> = ({ item, isHovering, onFavorite, onDelete }) => {
+  const controlClass = `controls ${ isHovering ? 'opacity-100' : 'opacity-0' } transition-opacity duration-200`;
+  return (
+    <div className={controlClass}>
+      <button
+        onClick={() => window.open(`/media/${item.file_name}`, '_blank')}
+        className="p-2 bg-gray-900 bg-opacity-75 rounded-full text-white hover:bg-opacity-90"
+      >
+        <Download className="w-4 h-4" />
+      </button>
+      <button
+        onClick={() => onFavorite(item)}
+        className="p-2 bg-gray-900 bg-opacity-75 rounded-full text-white hover:bg-opacity-90"
+      >
+        <Star className={`w-4 h-4 ${item.favorite ? 'fill-yellow-500' : ''}`} />
+      </button>
+      {!item.favorite && (
+        <button
+          onClick={() => onDelete(item)}
+          className="p-2 bg-gray-900 bg-opacity-75 rounded-full text-white hover:bg-opacity-90"
+        >
+          <Trash className="w-4 h-4" />
+        </button>
+      )}
+    </div>
+  );
+};
+
+type VideoImageSwitchProps = {
+  item: MediaItem;
+  setFullscreenItem: Function;
+};
+
+const VideoImageSwitch: React.FC< VideoImageSwitchProps> = ({ item, setFullscreenItem }) => {
+
+  if (['video'].includes(item.MediaType)) {
+    return <VideoItem item={item} />;
+  } else if (['image', 'photo'].includes(item.MediaType)) {
+    return (
+      <img
+        src={`/media/${item.file_name}`}
+        alt={item.file_name}
+        onClick={() => setFullscreenItem(item)}
+      />
+    );
+  }
+
+  return <div>Unsupported Media type: {}</div>;
+};
 
 const MediaCard: React.FC<MediaItemProps> = ({
   itemId,
@@ -120,19 +154,14 @@ const MediaCard: React.FC<MediaItemProps> = ({
     return (
       <div className="text-center text-red-600 p-4">
         <p>Error loading media: {error.message}</p>
-        <button
-          className="mt-2 px-4 py-2 bg-primary-600 text-white rounded hover:bg-primary-700"
-        >
+        <button className="mt-2 px-4 py-2 bg-primary-600 text-white rounded hover:bg-primary-700">
           Retry
         </button>
       </div>
     );
   }
 
-
-  const isVideo = ['video'].includes(item.MediaType);
-  const isImage = ['image', 'photo'].includes(item.MediaType);
-  const cardClass = 'media-item relative group' + (item.favorite ? ' favorite' : '')
+  const cardClass = 'media-item relative group' + (item.favorite ? ' favorite' : '');
 
   return (
     <div
@@ -140,20 +169,16 @@ const MediaCard: React.FC<MediaItemProps> = ({
       onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
     >
-      <div className={'media-item-content cursor-pointer'}>{
-          isVideo?
-            <VideoItem item={item} />
-          : isImage?
-            <img
-              src={`/media/${item.file_name}`}
-              alt={item.file_name}
-              onClick={() => setFullscreenItem(item)}
-            />
-          : null
-        }
+      <div className={'media-item-content cursor-pointer'}>
+        <VideoImageSwitch item={item} setFullscreenItem={setFullscreenItem} />
       </div>
       <MediaInfo item={item} />
-      <MediaControls item={item} isHovering={isHovering} onFavorite={onFavorite} onDelete={onDelete} />
+      <MediaControls
+        item={item}
+        isHovering={isHovering}
+        onFavorite={onFavorite}
+        onDelete={onDelete}
+      />
     </div>
   );
 };
