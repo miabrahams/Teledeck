6942920f5976953fbbcc00984a5d55a99aae9fc4
Michael Abrahams <miabraha@gmail.com>
2024-11-02 17:29:18 -0400

    Add consistent response types

--- web/src/shared/api/instance.ts
+++ web/src/shared/api/instance.ts
@@ -15,44 +15,44 @@ export const withPreferences = (route: ROUTE, preferences: Parameters<typeof cre
   return `${route}?${createPreferenceString(preferences)}`
 }
 
-export const getTotalPages = async (preferences: SearchPreferences) => {
+export const getTotalPages = async (preferences: SearchPreferences): Promise<number> => {
   const res = await instance.get(withPreferences(API_ENDPOINTS.total_pages, preferences));
-  return await res.data as number;
+  return await res.data;
 }
-export const getGalleryPage = async (preferences: SearchPreferences, page: number) => {
+export const getGalleryPage = async (preferences: SearchPreferences, page: number): Promise<MediaItem[]> => {
   const res = await instance.get(withPreferences(API_ENDPOINTS.gallery, {...preferences, page}));
-  return await res.data as MediaItem[];
+  return await res.data;
 }
 
 
-export const getGalleryIds = async (preferences: SearchPreferences) => {
+export const getGalleryIds = async (preferences: SearchPreferences): Promise<MediaID[]> => {
   const res = await instance.get(withPreferences(API_ENDPOINTS.gallery_ids, preferences));
-  return await res.data as MediaID[];
+  return await res.data;
 }
 
-export const getMediaItem = async (itemId: string) => {
+export const getMediaItem = async (itemId: string): Promise<MediaItem> => {
   const res = await instance.get(`/api/media/${itemId}`)
   return await res.data as MediaItem
 }
 
-export const getUser = async () => {
+export const getUser = async (): Promise<User> => {
   const res = await instance.get(`/api/me`)
-  return await res.data as User
+  return await res.data
 }
 
-export const postLogout = async () => {
+export const postLogout = async (): Promise<boolean> => {
   const res = await instance.post('/api/logout');
-  return res.data;
+  return res.status === 200;
 }
 
-export const postFavorite = async (itemId: string) => {
+export const postFavorite = async (itemId: string): Promise<MediaItem> => {
   const res = await instance.post(`/api/media/${itemId}/favorite`)
   return res.data as MediaItem;
 }
 
-export const deleteItem = async (itemId: string) => {
+export const deleteItem = async (itemId: string): Promise<boolean> => {
   const res = await instance.delete(`/api/media/${itemId}`)
-  return res
+  return res.status === 200;
 }
 
 
